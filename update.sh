12b8883af7016e8e4514d3c777790f089d4c1b32
Juntao Han <499445428@qq.com>
2018-10-22 12:46:10 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,8 +6,8 @@ if which gitbook > /dev/null; then
     cd source
     gitbook build
     cd _book
-    cp -R * ../../../interview-gitbook/
-    cd ../../../interview-gitbook/
+    cp -R * ../../../interview/
+    cd ../../../interview/
     git add -A
     git commit -am "update `date`"
     git push
